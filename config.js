5f14319521115e834d2bc14b7cba669893ea09ad
briteming <briteming@gmail.com>
2024-12-20 10:14:48 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ config = {
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     // 将配置好的 access token 拆分成两个字符串相加
-    // access_token: "abcdefg"+"hijklmn"
+    access_token: "ghp_NhyIYwdpDYx9OtWfa7ui"+"SYnnHmlwpQ3frOrZ"
 };
